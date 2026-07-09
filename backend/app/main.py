234ef47d06b38f2aa6352c1f59f7dcf2793fc480
Shreya Bisht <shreyabisht185@gmail.com>
2026-07-09 14:34:18 +0530

    Complete CRUD APIs and improve FastAPI backend

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,6 +1,17 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
+from pydantic import BaseModel
 
 app = FastAPI()
+
+docs = []
+
+class Documentation(BaseModel):
+    title: str
+    language: str
+    difficulty: str
+    content: str
+
+#routes
 
 @app.get("/")
 def home():
@@ -26,4 +37,51 @@ def get_user(user_id: int):
 def search(topic: str = "all"):
     return {
         "topic": topic
+    }
+
+@app.post("/docs", status_code=201)
+def create_documentation(doc: Documentation):
+    docs.append(doc)
+    return {
+        "message": "Documentation created successfully!",
+        "documentation": doc
+    }
+
+@app.get("/documentations")
+def get_all_documentation():
+    return docs
+
+@app.get("/documentations/{doc_id}", response_model=Documentation)
+def get_udocs(doc_id: int):
+
+    if doc_id < 0 or doc_id >= len(docs):
+        raise HTTPException(
+        status_code=404,
+        detail="Documentation not found"
+    )
+
+    return docs[doc_id]
+
+@app.delete("/documentations/{doc_id}")
+def delete_doc(doc_id:int):
+    if doc_id < 0 or doc_id >= len(docs):
+        raise HTTPException(
+        status_code=404,
+        detail="Documentation not found"
+    )
+    docs.pop(doc_id)
+    return {
+        "message": "Documentation deleted successfully!"
+    }
+
+@app.put("/documentations/{doc_id}")
+def update_doc(doc: Documentation, doc_id: int):
+    if doc_id < 0 or doc_id >= len(docs):
+        raise HTTPException(
+        status_code=404,
+        detail="Documentation not found"
+    )
+    docs[doc_id]=doc
+    return {
+        "message": "Documentation updated successfully!"
     }
